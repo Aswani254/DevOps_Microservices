9e677427447f644e3266733d98a6957455fd2743
EC2 Default User <ec2-user@ip-172-31-81-137.ec2.internal>
2022-09-05 14:09:26 +0000

    update:run_kubernetes.sh

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=solo254/microservices_project
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment hello-minikube --image=
+kubectl expose deployment hello-minikube --type=NodePort --port=8080
 
 # Step 3:
 # List kubernetes pods
+kubectl get services hello-minikube
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward service/hello-minikube 8000:80
