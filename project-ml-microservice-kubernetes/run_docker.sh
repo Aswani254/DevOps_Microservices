f800152f2c6721c8e812e1d55b44bd29421420ad
EC2 Default User <ec2-user@ip-172-31-81-137.ec2.internal>
2022-09-05 11:31:09 +0000

    moved ciccleci config,add upload_docker,add tag to run_docker

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -5,6 +5,7 @@
 # Step 1:
 # Build image and add a descriptive tag
 docker build ./ --tag=ap1
+docker tag ap1 solo254/ap1:ap1
 # Step 2: 
 # List docker images
 docker image ls
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -4,12 +4,14 @@
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+# Create dodockerckerpath
+dockerpath=solo254/microservices_project
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker push solo254/ap1:ap1
